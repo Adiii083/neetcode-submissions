f05b20e543d5b616079e77ef0e8ce754aa2d2a04
Aditya Kumar <121454493+Adiii083@users.noreply.github.com>
2026-06-05 09:17:10 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>>result;
+    void twoSum(vector<int>&nums, int target, int j, int k)
+    {
+        while(j<k)
+        {
+            if(nums[j]+nums[k]==target)
+        {
+            result.push_back({nums[j],nums[k],-target});
+            j++;
+            k--;
+            while(j<k && nums[j]==nums[j-1]) j++;
+            while(j<k && nums[k]==nums[k+1]) k--;
+
+        }
+        else if(nums[j]+nums[k]>target) k--;
+        else j++;
+
+        }
+        
+        
+    }
+
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+
+        for(int i=0;i<n-2;i++)
+        {
+            if(i>0 && nums[i]==nums[i-1]) continue;
+            int j=i+1;
+            int k=n-1;
+            int target=-nums[i];
+            twoSum(nums,target,j,k);
+        }
+        return result;
+            
+        
+    }
+};
